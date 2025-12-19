d6a0ebdd0b67769f5cf7042bcfba34c316142b02
Mahesh Jagale <maheshjagale93@gmail.com>
2025-12-19 14:17:23 +0530

    Update variables.tf

--- modules/project-factory/variables.tf
+++ modules/project-factory/variables.tf
@@ -1,15 +1,37 @@
+variable "org_id" {
+  description = "Organization ID"
+  type        = string
+}
+
 variable "project_id" {
-  description = "The GCP project ID"
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "project_name" {
+  description = "GCP Project Name"
   type        = string
 }
 
 variable "region" {
-  description = "The GCP region"
+  description = "Default region"
   type        = string
   default     = "us-central1"
 }
 
-variable "billing_account" {
-  description = "The billing account ID"
+variable "environment" {
+  description = "Environment name"
   type        = string
+}
+
+variable "enabled_apis" {
+  description = "List of APIs to enable"
+  type        = list(string)
+  default     = ["compute.googleapis.com", "container.googleapis.com"]
+}
+
+variable "labels" {
+  description = "Labels to apply"
+  type        = map(string)
+  default     = {}
 }
