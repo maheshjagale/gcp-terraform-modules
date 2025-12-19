a367dc6b752f4bf4cf6444d9b92380b759d3c7fa
Mahesh Jagale <maheshjagale93@gmail.com>
2025-12-19 12:52:49 +0530

    Add variables for GCP project configuration

---
+++ modules/project-factory/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "billing_account" {
+  description = "The billing account ID"
+  type        = string
+}
